a7170cfefd605ad5032fd926a9a0377d12face13
Dennis Jensen <ngh1adj95@hotmail.com>
2022-08-29 20:41:30 +0200

    Advancements:
    1. Added twilio for texting.
    2. Added a AWS secrets storage and enabled fetching it.
    3. Setup the script to run on a schedule.
    4. Wrapped the environment into docker but without AWS authentication.
    
    To do:
    1. Automatic deployment of environment to AWS.
    2. Schedule should only run in certain times of the day.

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,14 @@
-FROM python:3.11-slim
+FROM python:3.10-slim AS compile-image
+
+COPY requirements.txt requirements.txt
+RUN pip install --user -r requirements.txt
+
+FROM python:3.10-slim AS application-image
+WORKDIR /curly-girl
+COPY --from=compile-image /root/.local /root/.local
+COPY src src
+
+CMD ["python", "src/main.py"]
+
+
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+# Standard library
+from lib2to3.pgen2.token import OP
 from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import schedule
+
+# Application libraries
 from logger.init_logger import getLogger
+from aws_secrets import get_secret
+
+# Third party libraries
+from twilio.rest import Client
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 
 log = getLogger(__file__)
 
@@ -40,9 +49,7 @@ def extract_posts_from_page(driver: webdriver):
         elements = driver.find_elements(
             By.XPATH, "//*[@data-ad-preview='message']")
         log.debug("Found {} posts".format(len(elements)))
-        for i, element in enumerate(elements):
-            log.debug(
-                f"Post {i + 1} text: {element.text}")
+        for _, element in enumerate(elements):
             post_texts.append(element.text)
 
     except Exception as e:
@@ -52,25 +59,59 @@ def extract_posts_from_page(driver: webdriver):
     return post_texts
 
 
-def main():
+def send_message_to_user(secrets: dict):
+    account_sid = secrets["CURLY_TWILIO_ACCOUNT_SID"]
+    auth_token = secrets["CURLY_TWILIO_AUTH_TOKEN"]
+    from_number = secrets["TWILIO_FROM_NUMBER"]
+    to_number = secrets["ANNA_NUMBER"]
 
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+            body='Curly girl har et afbud! Skynd dig ind og book det.',
+            from_=from_number,
+            to=to_number
+        )
+
+    log.debug(f"Twillio message unique identifer: {message.sid}")
+
+
+def check_if_any_cancellation(secrets: dict, facebook_page: str):
     log.debug("Scrape the curly girl website")
-    facebook_page = "https://www.facebook.com/Vinthersklippekaelder"
-
-    driver = webdriver.Firefox()
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
     driver.get(facebook_page)
     driver.implicitly_wait(5)
-
     sleep(3)
     click_only_essential_cookies(driver)
     sleep(2)
     posts = extract_posts_from_page(driver)
     if check_if_any_post_is_cancellation(posts):
         log.info("Found a cancellation post - Notify the client")
+        send_message_to_user(secrets)
     else:
         log.info("Did not find a cancellation post - Do not notify the client")
-
     driver.close()
+
+
+def main():
+    facebook_page = "https://www.facebook.com/Vinthersklippekaelder"
+
+    no_error, secrets = get_secret()
+
+    if not no_error:
+        log.error("Unable to get TWILIO secrets")
+        return
+
+    schedule.every(1).minutes.do(
+        check_if_any_cancellation, secrets, facebook_page)
+
+    log.debug("Start the scheduler")
+    while True:
+        schedule.run_pending()
+        sleep(1)
 
 
 if __name__ == "__main__":
